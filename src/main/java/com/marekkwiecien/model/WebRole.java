5b2ae6336f593e2d7f4979fc7745ea79871223f1
kwiecienm <marek.kwiecien@gmail.com>
2018-06-07 20:19:32 +0200

    cleanup before version 2 (kotlin)

--- src/main/java/com/marekkwiecien/model/WebRole.java
+++
@@ -1,9 +0,0 @@
-package com.marekkwiecien.model;
-
-/**
- * Created by holow on 2/20/2017.
- */
-public final class WebRole {
-    public static final String ROLE_ADMIN = "ROLE_ADMIN";
-    public static final String ROLE_USER = "ROLE_USER";
-}
